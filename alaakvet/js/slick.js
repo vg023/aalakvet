3d46793cc718c9dd486c66dc32d8032246df3447
vg oa <victor@pugsarrollos.com>
2018-02-19 00:19:57 -0600

    Ajustes generales
    se integro la libreria de Slick, se integro SASS
    
    se comenzo la creacion del slider en los archivos 'slick' e 'index'

--- alaakvet/js/slick.js
+++ alaakvet/js/slick.js
@@ -1,6 +1,16 @@
 $(document).ready(function(e){
     $('.slider').slick({
       slidesToShow:1,
-      infinite:true
+      infinite:true,
+      autoplay: true,
+      autoplaySpeed:5000,
+      dots: true,
+      arrows: true,
+      fade:true
     });
+    $('.slider').on('click', function(event, slick, direction){
+      $(this).find()
+  // left
+});
+
 });
