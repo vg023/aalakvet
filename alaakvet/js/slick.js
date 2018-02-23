f65b7b6f7c669afe53d6787eb27245be8acb0793
victor <victor@pugsarrollos.com>
2018-02-22 19:46:01 -0600

    adicion animacion de slider
    se comenzo con la animacion del slider

--- alaakvet/js/slick.js
+++ alaakvet/js/slick.js
@@ -16,21 +16,35 @@ $(document).ready(function(e){
   $('.slider').on('beforeChange', function(event, slick, currentSlide, nextSlide){
     console.log( $(this).attr('class') );
     console.log( $(slick));
-    var tween = TweenMax.fromTo($(".img-slide-main"), 1,
-     		  {
-/*
-          x:-200,
-   */
-          y:-200
-       	  },
-     		  {
+    var tween = TweenMax.fromTo($(".img-slide-main"), 2,
+      {
+        x:-200,
+        y:-200
+     	},
+     	{
 /*
        		 x:0,
        		 y:0,
 */
-           opacity:1,
-     			 scale: 1,
-     			 ease: Power2.easeOut, y: 0
-     		  });
-     		  });
+        opacity:1,
+ 			   scale: 1,
+ 			   ease: Power2.easeOut, y: 0
+ 		   });
+ 		   var tween = TweenMax.fromTo($(".slidermask"), 2.5,
+      {
+        y:-200,
+        x:300
+     	},
+     	{
+/*
+       		 x:0,
+       		 y:0,
+*/
+        opacity:1,
+ 			   scale: 1,
+ 			   y:0,
+ 			   x:0
+ 		   });
     });
+     		  
+});
