ffd642e6d401799fc3ec8f618cbd52310bab41d7
victor <victor@pugsarrollos.com>
2018-02-22 19:35:46 -0600

    adicion de scripts
    se añadio script de scroll magic al codigo y se inserto animacion  de tweenmax en el slider de slick.

--- alaakvet/js/slick.js
+++ alaakvet/js/slick.js
@@ -3,10 +3,34 @@ $(document).ready(function(e){
       slidesToShow:1,
       infinite:true,
       autoplay: true,
-      autoplaySpeed:7000,
+      autoplaySpeed:2000,
       dots: true,
       arrows: true,
       pauseOnHover:false,
       fade:true
     });
-});
+    $('.slider').on('swipe', function(event, slick, direction){
+      // left
+     
+  });
+  $('.slider').on('beforeChange', function(event, slick, currentSlide, nextSlide){
+    console.log( $(this).attr('class') );
+    console.log( $(slick));
+    var tween = TweenMax.fromTo($(".img-slide-main"), 1,
+     		  {
+/*
+          x:-200,
+   */
+          y:-200
+       	  },
+     		  {
+/*
+       		 x:0,
+       		 y:0,
+*/
+           opacity:1,
+     			 scale: 1,
+     			 ease: Power2.easeOut, y: 0
+     		  });
+     		  });
+    });
