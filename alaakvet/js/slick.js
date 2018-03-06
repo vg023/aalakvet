12ef4c71f35246d0723e2d51c44a7b215e9cf2f6
vg oa <victor@pugsarrollos.com>
2018-03-06 02:08:42 -0600

    correccion animacion slider
    se comenzo la correcion del slider principal

--- alaakvet/js/slick.js
+++ alaakvet/js/slick.js
@@ -3,7 +3,7 @@ $(document).ready(function(e){
       slidesToShow:1,
       infinite:true,
       autoplay: true,
-      autoplaySpeed:2000,
+      autoplaySpeed:5000,
       dots: true,
       arrows: true,
       pauseOnHover:false,
@@ -11,24 +11,22 @@ $(document).ready(function(e){
     });
     $('.slider').on('swipe', function(event, slick, direction){
       // left
-     
+
   });
   $('.slider').on('beforeChange', function(event, slick, currentSlide, nextSlide){
     console.log( $(this).attr('class') );
     console.log( $(slick));
-    var tween = TweenMax.fromTo($(".img-slide-main"), 2,
+    var tween = TweenMax.fromTo($(".img-slide-main"), 2.5,
       {
-        x:-200,
-        y:-200
+        x:-500,
+        opacity:0
      	},
-     	{
-/*
-       		 x:0,
-       		 y:0,
-*/
+      {
         opacity:1,
  			   scale: 1,
+         x:0,
  			   ease: Power2.easeOut, y: 0
+
  		   });
  		   var tween = TweenMax.fromTo($(".slidermask"), 2.5,
       {
@@ -36,15 +34,11 @@ $(document).ready(function(e){
         x:300
      	},
      	{
-/*
-       		 x:0,
-       		 y:0,
-*/
         opacity:1,
  			   scale: 1,
  			   y:0,
  			   x:0
  		   });
     });
-     		  
+
 });
